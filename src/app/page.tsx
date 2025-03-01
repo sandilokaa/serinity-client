16d86bcc123d4edf76b76829d3470b4fc2ba6442
sandilokaa <sandyloka790@gmail.com>
2025-03-01 13:32:39 +0700

    docs: tidy up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import Header from "@/components/landing/Header";
 import Category from "@/components/landing/Category";
+import Item from "@/components/landing/Item";
+import Discover from "@/components/landing/Discover";
 
 export default function Home() {
   return (
     <main>
       <Header/>
       <Category/>
+      <Item/>
+      <Discover/>
     </main>
   );
 }
