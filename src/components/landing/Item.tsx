3f42196267d10ca7fdc1b78b97171569bf8bb1c8
sandilokaa <sandyloka790@gmail.com>
2025-03-01 13:33:16 +0700

    docs: tidy up

---
+++ src/components/landing/Item.tsx
@@ -0,0 +1,17 @@
+const Item: React.FC = () => {
+    return (
+        <div className="mt-24">
+            <div className="px-20">
+                <div className="flex flex-col gap-y-[10px]">
+                    <p className="font-medium text-4xl">New Arrivals</p>
+                    <p className="text-lg">Exciting New Pieces to Elevate Your Wardrobe</p>
+                </div>
+                <div className="mt-10">
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Item;
