9b315164ac4b13fc4144094c272a8d7fc0f3b21a
sandilokaa <sandyloka790@gmail.com>
2025-03-04 01:31:01 +0700

    docs: add components

---
+++ src/components/common/input/SubscribeInput.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function SubscribeInput() {
+    return (
+        <div className="flex items-center">
+            <input placeholder="Enter your email..." className="p-2 rounded-full border border-[#CACACA] w-[220px] placeholder:text-xs text-sm"/>
+        </div>
+    );
+}
