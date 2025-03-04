53fffcc6e9cdf5bf2b3d63e6b4f807877070066d
sandilokaa <sandyloka790@gmail.com>
2025-03-05 01:04:26 +0700

    docs: dispatch implementation

--- src/components/landing/Item.tsx
+++ src/components/landing/Item.tsx
@@ -1,6 +1,22 @@
-"use client";
+'use client';
+
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { AppDispatch } from "@/store/store";
+import { fetchData, selectSearchItemProducts, selectSearchItemProductsStatus } from "@/store/slice/SearchItemSlice";
+import CurrencyFormatter from "@/utils/CurrencyFormatter";
 
 const Item: React.FC = () => {
+
+    const dispatch = useDispatch<AppDispatch>();
+
+    const items = useSelector(selectSearchItemProducts);
+    const status = useSelector(selectSearchItemProductsStatus);
+
+    useEffect(() => {
+        dispatch(fetchData({ name: "", category: "" }));
+    }, [dispatch]);
+
     return (
         <div className="mt-24">
             <div className="px-20">
@@ -9,7 +25,16 @@ const Item: React.FC = () => {
                     <p className="text-lg">Exciting New Pieces to Elevate Your Wardrobe</p>
                 </div>
                 <div className="mt-10">
-                    
+                    {status === "loading" && <p>Loading...</p>}
+                    {status === "failed" && <p>Failed to fetch data</p>}
+                    <div className="grid grid-cols-3 gap-4">
+                        {items.map((item) => (
+                            <div key={item.id} className="border p-4 rounded-lg">
+                                <p className="text-xl font-semibold">{item.name}</p>
+                                <p className="text-green-600">{CurrencyFormatter(item.price)}</p>
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </div>
         </div>
