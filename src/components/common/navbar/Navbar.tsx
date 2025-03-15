e5f7fb56da17e1fb2f4e6c3d6b3e69a39632d6bb
sandilokaa <sandyloka790@gmail.com>
2025-03-15 23:37:07 +0700

    docs: set search modal at navbar

--- src/components/common/navbar/Navbar.tsx
+++ src/components/common/navbar/Navbar.tsx
@@ -1,22 +1,30 @@
 "use client";
 
 import Link from "next/link";
+import { useState } from "react";
+import SearchModal from "@/components/common/modal/SearchModal";
 
 const Navbar: React.FC = () => {
+    const [isSearchOpen, setIsSearchOpen] = useState(false);
+    
     return (
-        <nav className=" text-black py-5 px-20 flex justify-between items-center">
-            <Link href="/" className="text-[28px] font-bold">SERINITY</Link>
-            <div className="flex gap-x-[25px] items-center text-[18px] font-medium">
-                <Link href="/">Beranda -</Link>
-                <Link href="/shop">Shop -</Link>
-                <Link href="/support">Support -</Link>
-            </div>
-            <div className="flex gap-x-[20px] items-center text-[18px] font-medium">
-                <Link href="#">Search</Link>
-                <Link href="#">Bag<span className="text-xs">(0)</span></Link>
-                <Link href="#">Account<span className="text-xs">(0)</span></Link>
-            </div>
-        </nav>
+        <>
+            <nav className=" text-black py-5 px-20 flex justify-between items-center">
+                <Link href="/" className="text-[28px] font-bold">SERINITY</Link>
+                <div className="flex gap-x-[25px] items-center text-[18px] font-normal">
+                    <Link href="/">Beranda -</Link>
+                    <Link href="/shop">Shop -</Link>
+                    <Link href="/support">Support -</Link>
+                </div>
+                <div className="flex gap-x-[20px] items-center text-[18px] font-normal">
+                    <button onClick={() => setIsSearchOpen(true)}>Search</button>
+                    <Link href="#">Bag<span className="text-xs">(0)</span></Link>
+                    <Link href="#">Account<span className="text-xs">(0)</span></Link>
+                </div>
+            </nav>
+
+            { isSearchOpen && <SearchModal onClose={() => setIsSearchOpen(false)}/> }
+        </>
     );
 };
 
