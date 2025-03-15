18788ca0f1c54acf4c3d737b893d04a1e96eed0a
sandilokaa <sandyloka790@gmail.com>
2025-03-15 23:53:38 +0700

    docs: add reducer

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { createWrapper } from "next-redux-wrapper";
 import searchItemReducer from "./slice/SearchItemSlice";
+import newArrivalReducer from "./slice/NewArrivalSlice";
 
 const makeStore = () =>
     configureStore({
         reducer: {
-            searchItem: searchItemReducer
+            searchItem: searchItemReducer,
+            newArrival: newArrivalReducer,
         },
     });
 
